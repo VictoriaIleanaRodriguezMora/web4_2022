15d47701088028d604b5bf019cf1be751d13929a
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-10-24 17:47:38 -0300

    Desafio 3 Web 4 2022

---
+++ app.js
@@ -0,0 +1,6 @@
+const calc = require("./calculadora")
+
+console.log(calc.sumar(1, 1)); //2
+console.log(calc.restar(10, 1));//9
+console.log(calc.multiplicar(5, 5));//25
+console.log(calc.dividir(20, 2));//10
