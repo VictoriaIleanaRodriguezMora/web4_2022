cfd154b86f150476ad004dbd89cdaa744fdd55a8
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-10-24 17:13:44 -0300

    Desafio 2 Web 4 2022

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-// En este desafío deberás crear un algoritmo que tome un array, agregarle información (además de la que ya contiene) y crear una copia de este array. 
+const http = require("http")
+const PORT = 7000
+const server = http.createServer((req, res) => {
 
+    const url = req.url
 
-let villanosDeBatman = ['Joker', 'Bane', "Ra's Al Ghul"];
-villanosDeBatman = [...villanosDeBatman, 'Scarecrow']
+    if (url === "/") {
+        res.write(`
+        <!DOCTYPE html>
+        <html lang="en"> 
+        <head><meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+        <title>Desafio 2</title> 
+        </head> 
+        <body>`)
+        res.write(`
+        <form action="/bienvenida" method="post">
+         <label>Ingrese su nombre</label>
+         <input type="text" name="nombreUsuario" id="title" />
+         <input type="submit" value="ENVIAR" />
+        </form>`)
+        res.write(`</body> </html>`)
+    }
+    if (url === "/bienvenida") {
+        res.write(`
+        <!DOCTYPE html>
+        <html lang="en"> 
+        <head><meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+        <title>Desafio 2</title> 
+        </head> 
+        <body>`)
+        res.write(`
+      <h1> Bienvenid@ </h1>`)
+        res.write(`</body> </html>`)
+    }
+    return res.end()
 
-let copiaVillanos = [...villanosDeBatman]
-
-villanosDeBatman.map((element, index) => {
-    console.log("villanosDeBatman", element);
-    console.log("copiaVillanos", copiaVillanos[index])
 })
 
 
-// console.log(villanosDeBatman, copiaVillanos);
+
+
+
+server.listen(PORT, () => {
+    console.log(`Servidor Http escuchando en el puerto ${server.address().port}`)
+})
