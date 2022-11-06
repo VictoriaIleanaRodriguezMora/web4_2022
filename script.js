8650175404f07448cb5e22c3fa3b5acefda261f1
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-11-06 15:29:39 -0300

    D4

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// En este desafío deberás crear un algoritmo que tome un array, agregarle información (además de la que ya contiene) y crear una copia de este array. 
+// Consigna de desafío #4 {COMPLEMENTARIO}
+// Crear una app de Express que contenga un middleware que al ejecutarse escriba el siguiente mensaje por consola(terminarl)  “Ejecutando middleware”
 
 
-let villanosDeBatman = ['Joker', 'Bane', "Ra's Al Ghul"];
-villanosDeBatman = [...villanosDeBatman, 'Scarecrow']
-
-let copiaVillanos = [...villanosDeBatman]
-
-villanosDeBatman.map((element, index) => {
-    console.log("villanosDeBatman", element);
-    console.log("copiaVillanos", copiaVillanos[index])
-})
-
-
-// console.log(villanosDeBatman, copiaVillanos);
